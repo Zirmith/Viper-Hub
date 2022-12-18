4bf48195a4b6fa2e18b2b099e5a0d1fa9b1d6e27
_Headsetz <f2v74yt@gmail.com>
2022-12-18 13:41:03 -0600

    Added The Script

--- source/plugins/Viper_Util.lua
+++ source/plugins/Viper_Util.lua
@@ -1,1 +1,25 @@
+getgenv().Viper_Util = {
+    ['Players'] = game:GetService('Players')
+    ['UI'] = loadstring(game:HttpGet("https://raw.githubusercontent.com/deeeity/mercury-lib/master/src.lua"))(),
+    ['ReplicatedStore'] = game:GetService('ReplicatedStorage'),
+    ['Lighting'] = game:GetService('Lighting') or workspace.Lighting
+    ['UIS'] = game:GetService('UserInputService'),
+    ['Client'] = getgenv().Viper_Util['Players'].LocalPlayer,
+    ['CV'] = loadstring(game:HttpGet("https://raw.githubusercontent.com/deeeity/viper-hub/master/VERSION.lua"))()
+}
 
+
+getgenv().Viper_Util['Char'] = getgenv().Viper_Util['Client'].Character
+
+getgenv().Viper_Util:CheckForUpdate()
+    if getgenv().Viper_Ui['Version'] == getgenv().Viper_Util['CV'].Version then
+        return 'Already Updated'
+    else
+        pcall(function()
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/deeeity/viper-hub/master/source/main/public/builds/"..getgenv().Viper_Util['CV'].Version))()
+        end)
+        return 'Updated'
+    end
+end
+
+return getgenv().Viper_Util
